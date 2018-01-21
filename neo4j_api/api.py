102cf3d6b08ce509bbae4e437b0dd348f6ce40af
Jason <jasonjimenezcruz@gmail.com>
2018-01-21 10:07:32 +0100

    Added main template

--- neo4j_api/api.py
+++ neo4j_api/api.py
@@ -1,9 +1,9 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 from config import neo_credentials, debug
-from model import GraphManager
+#from model import GraphManager
 import json
 
-graph = GraphManager(neo_credentials)
+#graph = GraphManager(neo_credentials)
 
 app = Flask(__name__)
 
@@ -12,6 +12,12 @@ def json_home():
     jresponse = {"msj": "Hello World!"}
     r = Response(json.dumps(jresponse), status=200, mimetype="application/json")
     return r
+
+
+@app.route('/test_template')
+def test_template():
+    return render_template('template.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=debug)
