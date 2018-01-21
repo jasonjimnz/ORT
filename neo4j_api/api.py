9e8fd1b61d96523de56689ecb2ede16dc84b8d53
Jason Jiménez Cruz <jasonjimenezcruz@gmail.com>
2018-01-21 13:27:43 +0100

    Finished basic search algorithms

--- neo4j_api/api.py
+++ neo4j_api/api.py
@@ -1,9 +1,9 @@
 from flask import Flask, Response, render_template
 from config import neo_credentials, debug
-#from model import GraphManager
+from model import GraphManager
 import json
 
-#graph = GraphManager(neo_credentials)
+graph = GraphManager(neo_credentials, "6Q3Gzq", "187170","4FBF01FF-390C-46AB-A3CF-D0827CAD86B9","Team4")
 
 app = Flask(__name__)
 
@@ -16,7 +16,8 @@ def json_home():
 
 @app.route('/test_template')
 def test_template():
-    return render_template('template.html')
+    cities = graph.get_cities()
+    return render_template('template.html', cities=cities)
 
 
 if __name__ == "__main__":
--- neo4j_api/test_model_interface.py
+++ neo4j_api/test_model_interface.py
@@ -1,35 +1,46 @@
 import random
 
+import requests
 from faker import Faker
 
-fake = Faker()
+fake = Faker('es_ES')
 class PersonGenerator:
     faker_instance = fake
+    minube_api_key = None
+    cities_list = []
 
-    def __init__(self):
+    def __init__(self, minube_api_key, cities=None):
+        self.minube_api_key = minube_api_key
+        if cities:
+            self.cities_list = cities
         print("Initialized")
 
-    @classmethod
+
     def get_person(cls):
         return {
             "name": cls.faker_instance.name(),
             "last_name": cls.faker_instance.last_name()
         }
 
-    @classmethod
-    def get_city(cls):
-        return {
-            "city": cls.faker_instance.city(),
-        }
 
-    @classmethod
+    def get_city(self):
+        if self.cities_list:
+            return {
+                "city": self.cities_list[random.randint(0, len(self.cities_list)-1)]
+            }
+        else:
+            return {
+                "city": self.faker_instance.city(),
+            }
+
+
     def get_single_travel(cls):
         return {
             "city": cls.faker_instance.city(),
             "date": cls.faker_instance.date_time_this_decade().strftime('%Y-%m-%d')
         }
 
-    @classmethod
+
     def get_preson_travels(cls):
         person_travels = {
             "person": cls.get_person(),
@@ -39,7 +50,7 @@ class PersonGenerator:
 
         return person_travels
 
-    @classmethod
+
     def get_activity(cls):
         activities = [
             "Goes to a museum",
@@ -75,3 +86,5 @@ class PersonGenerator:
         ]
 
         return purposes[random.randint(0, len(purposes)-1)]
+
+
